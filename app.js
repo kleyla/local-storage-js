a0535c3b65a46b93cee2781a09e344e8b4f4c7d6
kleyla <kleyla01@live.com>
2020-03-31 16:06:26 -0400

    local storage

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ function eventListeners() {
     .querySelector("#formulario")
     .addEventListener("submit", agregarTweet);
   listaTweets.addEventListener("click", borrarTweet);
+  //   Tweets cargados
+  document.addEventListener("DOMContentLoaded", localStorageListo);
 }
 
 function agregarTweet(e) {
@@ -26,13 +28,61 @@ function agregarTweet(e) {
   li.innerText = tweet;
   li.appendChild(btnBorrar);
   listaTweets.appendChild(li);
-  console.log(tweet);
+
+  //   Agregar a local storage
+  agregarTweetLocalStorage(tweet);
 }
 
 function borrarTweet(e) {
   e.preventDefault();
   if (e.target.className === "borrar-tweet") {
-    console.log(e.target.parentElement.remove());
-    alert("Tweet eliminado!")
+    e.target.parentElement.remove();
+    // alert("Tweet eliminado!");
+    // console.log(e.target.parentElement.innerText);
+    borrarTweetLocalStorage(e.target.parentElement.innerText);
   }
+}
+function agregarTweetLocalStorage(tweet) {
+  let tweets;
+  tweets = getTweetLocalStorage();
+  tweets.push(tweet);
+  localStorage.setItem("tweets", JSON.stringify(tweets));
+}
+
+function getTweetLocalStorage() {
+  let tweets;
+  if (localStorage.getItem("tweets") === null) {
+    tweets = [];
+  } else {
+    tweets = JSON.parse(localStorage.getItem("tweets"));
+  }
+  return tweets;
+}
+function localStorageListo() {
+  let tweets;
+  tweets = getTweetLocalStorage();
+  //   console.log(tweets);
+  tweets.forEach(function(tweet) {
+    const btnBorrar = document.createElement("a");
+    btnBorrar.classList = "borrar-tweet";
+    btnBorrar.innerText = "X";
+
+    const li = document.createElement("li");
+    li.innerText = tweet;
+    li.appendChild(btnBorrar);
+    listaTweets.appendChild(li);
+  });
+}
+function borrarTweetLocalStorage(tweet) {
+  let tweetBorrar;
+  tweetBorrar = tweet.substring(0, tweet.length - 1);
+  let tweets = getTweetLocalStorage();
+  tweets.forEach(function(tweet, index) {
+    // console.log(tweet);
+    if (tweet === tweetBorrar) {
+      tweets.splice(index, 1);
+    }
+  });
+  //   console.log(tweets);
+  localStorage.setItem("tweets", JSON.stringify(tweets));
 }
